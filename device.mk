bccac1ba497afa851ab851b0673175ef53808d9d
Userj96 <115395855+Userj96@users.noreply.github.com>
2024-09-25 04:29:27 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -51,7 +51,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.2-impl.recovery \
